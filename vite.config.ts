14baebb19e838166d2c4060db7e664c2c05f97b6
sarthak027 <sarthakprasad012@gmail.com>
2025-06-10 14:50:23 +0530

    deployment changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,17 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  
   plugins: [react()],
-  base: "/Digital_store",
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   },
 });
